bf86c7c2858e686d6d7a42e4c824fb413cf78258
gendro <unikgendro@hotmail.com>
2016-04-11 14:18:18 -0600

    Commit 6

---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+function ejecutar(){
+	function onRequest(req, res){
+		var rutaArchivo = '.' + ((req.url=='/')?'/serrano.html':req.url);
+		console.log(rutaArchivo);
+		
+		var ext = path.extname(rutaArchivo);
+		var contentType = 'text/html';
+		switch(ext){
+			case '.css':
+				contentType = 'text/css';
+				break;
+			case '.js':
+				contentType = 'text/javascript';
+				break;
+		}
+		
+		path.exists(rutaArchivo, function(existe){
+			if(existe){
+				fs.readFile(rutaArchivo, function(error, contenido){
+					if(error){
+						res.writeHead(500);
+						res.end();
+					}else{
+						res.writeHead(200, {'Content-Type':contentType});
+						res.end(contenido);
+					}
+				})
+			}else{
+				res.writeHead(404);
+				res.end();
+			}
+		});
+	}
+	var server = http.createServer(onRequest).listen(5000);
+	console.log('Servidor corriendo en puerto 5000')
+}
+
+
+exports.ejecutar = ejecutar;
